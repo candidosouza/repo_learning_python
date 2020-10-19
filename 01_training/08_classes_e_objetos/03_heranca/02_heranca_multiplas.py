08632a3b1dcea2e6e4b92ce58573e27cf7c6be64
Candido Souza <candidosouzza@gmail.com>
2020-10-19 11:57:11 -0300

    herança multiplas

---
+++ 01_training/08_classes_e_objetos/03_heranca/02_heranca_multiplas.py
@@ -0,0 +1,25 @@
+"""
+Herança multipla, é aceita em python,
+caso as classes tenham os mesmo método, prevalecerá a colocada em primeiro logar
+"""
+
+class A():
+    
+    def info(self) -> str:
+        print("Hedada da A")
+
+
+class B():
+    
+    def info(self) -> str:
+        print("Hedada da B")
+
+
+class C(A, B):
+    
+    def __init__(self):
+        self.init = True
+
+
+heranca = C()
+heranca.info()
