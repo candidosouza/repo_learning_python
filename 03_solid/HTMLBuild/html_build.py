ca4ded943b12e408480086d206160c2c5addc7fd
Candido Souza <candidosouzza@gmail.com>
2020-10-21 10:49:21 -0300

    alteração no html_build.py

--- 03_solid/HTMLBuild/html_build.py
+++ 03_solid/HTMLBuild/html_build.py
@@ -1,8 +1,29 @@
-
-class HTMLBuild():
-    pass
+class Link():
+    """Destinada a criação de tags de links"""
     
+    __slots__ = ['__href', '__title']
     
+    def __init__(self, href: str = '', title: str = ''):
+        self.__href: str = href
+        self.__title: str = title
+    
+    @property
+    def href(self):
+        return self.__href
+    
+    @property
+    def title(self):
+        return self.__title
+    
+    def render(self) -> str:
+        if self.__title == '':
+            return f'<a href="{self.href}" title="{self.href}">'
+        return f'<a href="{self.href}" title="{self.title}">'
+class HTMLBuild(object):
+    
+    def __getattribute__(self, name, **kwargs):
+        return eval(name.title())().render()
 
 
 html = HTMLBuild()
+print(html.link)
